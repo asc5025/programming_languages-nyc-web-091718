c4cb3cd4e701b181846f87d234f5efda779f2ec0
Albert Chun <albert.s.chun@gmail.com>
2018-09-12 15:09:58 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each do |oo_functional, lang|
+     lang.each do |name, type|
+       new_languages[name] ||= type
+       # if new_languages[name] is nil or false sets type as the key value pair for name as the nested hash
+       new_languages[name][:style] ||= []
+       # if new_languages[name][:style] is nil or false, sets it to an elements array for value of style key
+       new_languages[name][:style] << oo_functional
+     end
+   end
+   new_languages
 end
